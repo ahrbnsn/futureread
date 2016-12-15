892257d58c4d70eee09edc9a1f8f45cfc928b873
Ashley Robinson <ashleyhowerobinson@gmail.com>
2016-12-14 19:54:25 -0500

    Cleaned it up; added ability to delete last add & select from a list

--- bin/run.rb
+++ bin/run.rb
@@ -1,9 +1,18 @@
 #!/usr/bin/env ruby
 require_relative '../config/environment'
 
-BookDate.request_book
-BookDate.authenticate
-BookDate.request_book_id
-BookDate.add_to_shelf
-BookDate.undo?
+BookDate.create_token
+puts "What would you like to read at some point?"
+input = gets.chomp
+book = BookDate.new(input)
+book.retrieve_results
+book.add_top_pick
+book.correct?
+
+
+# BookDate.request_book
+# BookDate.authenticate
+# BookDate.request_book_id
+# BookDate.add_to_shelf
+# BookDate.undo?
 
--- lib/book_date.rb
+++ lib/book_date.rb
@@ -1,44 +1,131 @@
 class BookDate
+  
+  KEY        = 
+  APP_SECRET = ENV['GR_SECRET']
+  TOKEN      = ENV['TOKEN']
+  SECRET     = ENV['SECRET']
+  SEARCH_URL = "https://www.goodreads.com/search/index.xml"
 
-# get oath code
-# get input
-# make request to goodreads
-
-
-  def self.request_book
-    puts "What do you want to read?"
-    @@book = gets.strip
+  def self.creds 
+    {:key       => ENV['GR_KEY'],
+    :sec        => ENV['GR_SECRET'],
+    :token      => ENV['TOKEN'],
+    :secret     => ENV['SECRET'],
+    :search_url => "https://www.goodreads.com/search/index.xml"}
+  end
+  
+  def self.create_token
+    consumer       = OAuth::Consumer.new(creds[:key], creds[:sec], :site => 'http://www.goodreads.com')
+    @@access_token = OAuth::AccessToken.new(consumer, creds[:token], creds[:secret])
   end
 
-  def self.authenticate
-    the_token = ENV['TOKEN']
-    the_secret = ENV['SECRET']
-    consumer = OAuth::Consumer.new(ENV['GR_KEY'],
-                               ENV['GR_SECRET'],
-                               :site => 'http://www.goodreads.com')
-    @@access_token = OAuth::AccessToken.new(consumer, the_token, the_secret)
-  end
-
-  def self.request_book_id
-    param = URI::encode(@@book)
-    uri = URI.parse "https://www.goodreads.com/search/index.xml?key=#{ENV['GR_KEY']}&q=#{param}"
-    response = Timeout::timeout(10) { Net::HTTP.get(uri) }
-    doc = Nokogiri.XML(response)
-
-    @@book_id = doc.css("search results best_book id").first.text
-    @@author = doc.css("search results best_book author name").first.text
-    @@book_title = doc.css("best_book title").first.text
+  def self.token
+    @@access_token
   end
 
 
-  def self.add_to_shelf
-    self.access_token.post('/shelf/add_to_shelf.xml', 
-             {'book_id' => book_id,
-             'name' => 'to-read'},
+  attr_accessor :query, :results, :last_added
+
+  def initialize(query)
+    @query       = query
+  end
+
+  def retrieve_results
+    param        = URI::encode(query)
+    uri          = URI.parse "#{SEARCH_URL}?key=#{ENV['GR_KEY']}&q=#{param}"
+    response     = Timeout::timeout(10) { Net::HTTP.get(uri) }
+    self.results = Nokogiri.XML(response)
+  end
+
+  def book_results
+    books    = results.css("search work")
+    books.collect do |book|
+      author = book.at_css("best_book author name").text
+      title  = book.at_css("best_book title").text
+      id     = book.at_css("best_book id").text 
+      
+      {:author => author,
+       :title  => title,
+       :id     => id}
+     end
+  end
+
+  def top_pick 
+    book_results[0]
+  end
+
+  def add_top_pick
+    add_to_shelf(top_pick)
+  end
+
+  def add_to_shelf(pick)
+    token.post('/shelf/add_to_shelf.xml', 
+              {'book_id' => pick[:id], 'name' => 'to-read'},  
               { 'Accept'=>'application/xml', 'Content-Type' => 'application/xml' 
               })
-    puts "Added #{@@book_title} by #{@@author}"
+    self.last_added = pick
+    puts "Added #{pick[:title]} by #{pick[:author]}"
   end
+
+  def correct?
+    puts "That what you wanted?"
+    answer = gets.chomp
+    unless answer.downcase.include?("y")
+      remove_from_shelf!
+      list_all_books
+      pick_new_book
+    end
+  end
+
+  def remove_from_shelf!
+    token.post('/shelf/add_to_shelf.xml', 
+              {'book_id' => last_added[:id], 'name' => 'to-read', 'a' => 'remove'},  
+              { 'Accept'=>'application/xml', 'Content-Type' => 'application/xml' 
+              })
+    puts "Removed #{last_added[:title]} by #{last_added[:author]}"
+    last_added[:removed] = true
+  end
+
+  def list_all_books
+    book_results.each.with_index(1) do |book, i|
+      puts "#{i}. #{book[:title]} | #{book[:author]} "
+    end
+  end
+
+  def pick_new_book
+    puts "What would you like to add?"
+    selection = gets.chomp.to_i
+    index = selection.to_i - 1
+    add_to_shelf(book_results[index])
+  end
+
+
+  def token
+    self.class.token
+  end
+
+
+
+  # def self.request_book_id
+  #   param = URI::encode(@@book)
+  #   uri = URI.parse "#{SEARCH_URL}?key=#{ENV['GR_KEY']}&q=#{param}"
+  #   response = Timeout::timeout(10) { Net::HTTP.get(uri) }
+  #   doc = Nokogiri.XML(response)
+
+  #   @@book_id = doc.css("search results best_book id").first.text
+  #   @@author = doc.css("search results best_book author name").first.text
+  #   @@book_title = doc.css("best_book title").first.text
+  # end
+
+
+  # def self.add_to_shelf
+  #   self.access_token.post('/shelf/add_to_shelf.xml', 
+  #            {'book_id' => book_id,
+  #            'name' => 'to-read'},
+  #             { 'Accept'=>'application/xml', 'Content-Type' => 'application/xml' 
+  #             })
+  #   puts "Added #{@@book_title} by #{@@author}"
+  # end
 
   def self.undo?
     puts "Wrong choice?"
